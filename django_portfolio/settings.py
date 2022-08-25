10e3cdaa8273112f86a83a320da9d885e71f8b38
Joe Shepard <shepard.joseph.m@gmail.com>
2022-08-25 09:28:47 -0700

    allowed hosts

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -17,7 +17,9 @@ SECRET_KEY=env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = ['jms-portfolio.herokuapp.com']
 
 
 # Application definition
